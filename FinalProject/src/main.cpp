01f596d888dadd5ca350d8e8a9187581bec53050
lekhovitsky <lekhovitsky@gmail.com>
2020-05-29 15:29:58 +0300

    aho-corasick implementation

--- FinalProject/src/main.cpp
+++ FinalProject/src/main.cpp
@@ -1,3 +1,19 @@
+#include <string>
+#include <iostream>
+#include "utils.h"
+#include "aho_corasick.h"
+
+
 int main(int argc, char **argv) {
+    // auto fastas = read_fasta("../f.txt");
+
+    std::vector<std::string> patterns = {"he", "she", "hers", "his"};
+    std::string text = "ahishers";
+
+    AhoCorasick ac;
+
+    ac.set_patterns(patterns);
+    ac.match(text);
+
     return 0;
 }
