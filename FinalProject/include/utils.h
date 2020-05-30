7c529349c8e45c1ce8fc59c6cd83f5d73267e09a
lekhovitsky <lekhovitsky@gmail.com>
2020-05-30 19:26:29 +0300

    some small modifications

--- FinalProject/include/utils.h
+++ FinalProject/include/utils.h
@@ -10,13 +10,11 @@ struct FastaRecord
     std::string _id{};
     std::string _sequence{};
 
-    FastaRecord(
-            std::string id,
-            std::string sequence)
+    explicit FastaRecord(
+            std::string id = "",
+            std::string sequence = "")
         : _id{std::move(id)}
         , _sequence{std::move(sequence)} {};
-
-    FastaRecord(): _id{""}, _sequence{""} {};
 };
 
 struct MarkerRecord
@@ -24,34 +22,34 @@ struct MarkerRecord
     std::string _id{};
     std::string _marker{};
 
-    MarkerRecord(
-            std::string id,
-            std::string marker)
+    explicit MarkerRecord(
+            std::string id = "",
+            std::string marker = "")
         : _id{std::move(id)}
         , _marker{std::move(marker)} {};
 };
 
-
-// file manipulations
+// reading genomes and markers
 template<typename Stream>
 std::vector<FastaRecord>    read_fasta(Stream &stream);
 std::vector<FastaRecord>    read_fasta_file(const std::string &file_name);
-std::vector<FastaRecord>    read_fasta_string(const std::string &text);
-std::vector<FastaRecord>    read_archive(const std::string &file_name);
+std::vector<FastaRecord>    read_fasta_archive(const std::string &file_name);
 std::vector<MarkerRecord>   read_markers(const std::string& file_name, size_t max_rows = 0);
-std::vector<size_t>         split_file(const std::string& file, size_t n_chunks);
-bool                        is_archive(const std::string &file_name);
-bool                        is_csv_file(const std::string& file_name);
-bool                        is_text_file(const std::string& file_name);
-bool                        is_fasta_file(const std::string& file_name);
 
-void                        write_result(
+// writing AC results
+void write_result(
         const std::string                    &file_name,
         const std::vector<std::vector<bool>> &result,
         const std::vector<std::string>       &sequence_ids,
-        const std::vector<std::string>       &marker_ids
-);
+        const std::vector<std::string>       &marker_ids);
 
+// file extension check routines
+bool is_archive(const std::string &file_name);
+bool is_csv_file(const std::string& file_name);
+bool is_text_file(const std::string& file_name);
+bool is_fasta_file(const std::string& file_name);
+
+// fasta constants
 const std::string FASTA_CHARS {"ACGTURYKMSWBDHVN"};
 constexpr char FASTA_COMMENT_START {';'};
 constexpr char FASTA_ID_START {'>'};
