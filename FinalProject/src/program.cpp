77b7b3e763ed6a95998d090f4cef4555bb928757
lekhovitsky <lekhovitsky@gmail.com>
2020-06-01 12:13:05 +0300

    refactor Program::run(): separate into multiple methods

--- FinalProject/src/program.cpp
+++ FinalProject/src/program.cpp
@@ -28,56 +28,72 @@ Program::Program(
         _matchers.emplace_back(_ac, _q, _m);
 }
 
-void Program::run() {
-    //
-    // prepare markers and AC; maybe extract method?
-    //
-    auto m_read_start = Time::now();
-    auto markers = read_markers(_markers_file);
-    auto m_read_end = Time::now();
 
-    std::vector<std::string> m_ids, patterns;
-    m_ids.reserve(markers.size());
+void Program::run() {
+    prepare();
+    execute();
+    save();
+    report();
+    cleanup();
+}
+
+
+void Program::prepare() {
+    // read markers
+    auto read_start = Time::now();
+    auto markers = read_markers(_markers_file);
+    auto read_end = Time::now();
+    _markers_reading_time = Time::diff(read_start, read_end);
+
+    // save marker ids, prepare patterns
+    std::vector<std::string> patterns;
+    _m_ids.reserve(markers.size());
     patterns.reserve(markers.size());
     for (auto &&m : markers) {
-        m_ids.push_back(std::move(m._id));
+        _m_ids.push_back(std::move(m._id));
         patterns.push_back(std::move(m._marker));
     }
     markers.clear();
 
+    // prepare Aho-Corasick trie
     auto build_start = Time::now();
     _ac.set(std::move(patterns));
     auto build_end = Time::now();
+    _trie_building_time = Time::diff(build_start, build_end);
+}
 
-    //
-    // launch parallel execution
-    //
-    // launch reader; maybe extract method?
-    auto s_read_start = Time::now();
+
+void Program::execute() {
+    // launch reader
+    auto read_start = Time::now();
     std::thread reader_thread(&SequenceReader::run, &_reader);
 
-    // launch matchers; maybe extract method?
+    // launch matchers
     std::vector<std::thread> matcher_threads;
     matcher_threads.reserve(_matchers.size() + 1);
     auto match_start = Time::now();
     for (auto& m : _matchers)
         matcher_threads.emplace_back(&SequenceMatcher::run, &m);
 
-    // wait for threads to finish
+    // wait for reader thread to finish
     reader_thread.join();
-    auto s_read_end = Time::now();
+    auto read_end = Time::now();
+    _genomes_reading_time = Time::diff(read_start, read_end);
 
     // redirect the new free thread
     _matchers.emplace_back(_ac, _q, _m);
     matcher_threads.emplace_back(&SequenceMatcher::run, &_matchers.back());
 
+    // wait for matcher threads to finish
     for (auto& t : matcher_threads)
         t.join();
     auto match_end = Time::now();
+    _markers_matching_time = Time::diff(match_start, match_end);
+}
 
-    //
-    // save results to csv; maybe extract method?
-    //
+
+void Program::save() {
+    // extract results from our map
     std::vector<std::string> s_ids;
     s_ids.reserve(_m.size());
     std::vector<std::vector<bool>> result;
@@ -86,19 +102,31 @@ void Program::run() {
         s_ids.push_back(id);
         result.push_back(std::move(res));
     }
-    _m.clear();
 
+    // save results
     auto write_start = Time::now();
-    write_result(_result_file, result, s_ids, m_ids);
+    write_result(_result_file, result, s_ids, _m_ids);
     auto write_end = Time::now();
+    _results_saving_time = Time::diff(write_start, write_end);
+}
 
+
+void Program::report() const {
     if (_verbose) {
         std::cout
                 << std::setprecision(3) << std::fixed
-                << "Reading markers:   " << Time::diff(m_read_start, m_read_end) << " seconds\n"
-                << "Building trie:     " << Time::diff( build_start,  build_end) << " seconds\n"
-                << "Reading sequences: " << Time::diff(s_read_start, s_read_end) << " seconds\n"
-                << "Matching patterns: " << Time::diff( match_start,  match_end) << " seconds\n"
-                << "Writing results:   " << Time::diff( write_start,  write_end) << " seconds\n";
+                << "Reading markers:  " << _markers_reading_time << " seconds\n"
+                << "Building trie:    " << _trie_building_time << " seconds\n"
+                << "Reading genomes:  " << _genomes_reading_time << " seconds\n"
+                << "Matching markers: " << _markers_matching_time << " seconds\n"
+                << "Saving results:  " << _results_saving_time << " seconds\n";
     }
+}
+
+
+void Program::cleanup() {
+    _m_ids.clear();
+    _ac.reset();
+    _m.clear();
+    _q.clear();
 }
